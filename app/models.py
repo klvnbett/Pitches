225d2bc96ceef6046a02c524c4b0cad8f48e1476
BettK <kelvin.bett@student.moringaschool.com>
2021-08-17 08:48:20 +0300

    added class pitch

--- app/models.py
+++ app/models.py
@@ -38,3 +38,31 @@ class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     comment = db.Column(db.String(255))
     users = db.relationship('User', backref='comment', lazy="dynamic")
+
+class Pitch(db.Model):
+    __tablename__ = 'pitches'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    pitch_id = db.Column(db.Integer)
+    pitch_title = db.Column(db.String)
+    pitch_category = db.Column(db.String)
+    pitch_comment = db.Column(db.String)
+    posted = db.Column(db.DateTime, default=datetime.utcnow)
+    likes = db.Column(db.Integer)
+    dislikes = db.Column(db.Integer)
+
+    comments = db.relationship('Comment', backref='pitch', lazy="dynamic")
+
+    def save_pitch(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_pitches(cls, category):
+        pitches = Pitch.query.filter_by(pitch_category=category).all()
+        return pitches
+
+    @classmethod
+    def getPitchId(cls, id):
+        pitch = Pitch.query.filter_by(id=id).first()
+        return pitch
--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+
 class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:read432q@localhost/pitch'
     SECRET_KEY= os.environ.get("SECRET_KEY")
@@ -6,16 +7,30 @@ class Config:
     UPLOADED_PHOTOS_DEST='app/static/photos'
 
     #email configuration
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
-    class DevConfig(config):
-        Debug =True
-
-
-    class ProdConfig(Config):
-        pass
-
-    Config_options ={
-        'development':DevConfig,
-        'production' :ProdConfig
-    }
+    UPLOADED_PHOTOS_DEST='app/static/photos'
     
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    
+class DevConfig(Config):
+    Debug = True
+
+
+class ProdConfig(Config):
+    pass
+
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig
+}
