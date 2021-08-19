97d2b25107eaeda44a7318fb96729422e03eb232
BettK <kelvin.bett@student.moringaschool.com>
2021-08-19 15:21:39 +0300

    updated home page

--- app/main/views.py
+++ app/main/views.py
@@ -82,30 +82,15 @@ def new_pitch():
         all_pitches = Pitch.query.order_by(Pitch.posted).all()
     
     return render_template('newpitch.html', pitches=all_pitches,form_pitch = form_pitch)
+  
 
-@main.route('/comment/<int:id>',methods= ['POST','GET'])
+@main.route('/<int:id>',methods= ['POST','GET'])
 @login_required
 def viewPitch(id):
     eachpitch = Pitch.getPitchId(id)
     comments = Comment.getComments(id)
 
-    if request.args.get("like"):
-        eachpitch=0;
-        eachpitch.likes = eachpitch.likes + 1
-
-        db.session.add(eachpitch)
-        db.session.commit()
-
-        return redirect(eachpitch)
-
-    elif request.args.get("dislike"):
-        eachpitch=0;
-
-        db.session.add(eachpitch)
-        db.session.commit()
-
-        return redirect(eachpitch)
-
+    
     commentForm = CommentForm()
     if commentForm.validate_on_submit():
         comment = commentForm.text.data
@@ -116,24 +101,20 @@ def viewPitch(id):
 
     return render_template('comment.html',commentForm = commentForm,comments = comments,pitch = eachpitch)
 
-
-
-@main.route('/category/product',methods= ['GET'])
-def displayProductCategory():
-    productPitches = Pitch.get_pitches('product')
-    return render_template('category/product.html',productPitches = productPitches)
+@main.route('/comment/<int:id>', methods=['POST'])
+@login_required
+def comment(id):
+    comment_form = CommentForm(id)
+    if comment_form.validate_on_submit():
+        pitch_comment = comment_form.pitch_comment.data
+        new_comment = Comment(pitch_comment=pitch_comment, user=current_user)
+        
+        new_comment.saveComment()
+        db.session.add(new_comment)
+        db.session.commit()
+        
+        return redirect(url_for('main.index'))
+    else:
+        all_comments = Comment.query.order_by(Comment.posted).all()
     
-@main.route('/category/promotion',methods= ['POST','GET'])
-def displaypromotionCategory():
-    promotionPitches = Pitch.get_pitches('promotion')
-    return render_template('category/promotion.html',promotionPitches = promotionPitches)
-
-@main.route('/category/business',methods= ['POST','GET'])
-def displaybusinessCategory():
-    businessPitches = Pitch.get_pitches('business')
-    return render_template('category/business.html',businessPitches = businessPitches)
-
-@main.route('/category/pickup',methods= ['POST','GET'])
-def displayPickupCategory():
-    pickupPitches = Pitch.get_pitches('pickup')
-    return render_template('category/pickup.html',pickupPitches = pickupPitches)
+    return render_template('comment.html', pitches=all_comments,comment_form = comment_form)
